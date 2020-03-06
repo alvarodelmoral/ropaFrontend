6116bb0127aaf1d54b497756acdf563feb33450e
alvarodelmoral <ama.99@hotmail.es>
2020-03-06 13:45:07 +0100

    first commit

---
+++ src/Contenido.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { Route }  from "svelte-routing";
+  import Inicio     from "./Inicio.svelte";
+  import Prendas    from "./Prendas.svelte";
+  import Clientes   from "./Clientes.svelte";
+</script>
+
+<style>
+  main {
+    padding: 20px;
+    padding-top: 50px;
+  }
+</style>
+
+<main id="contenido">
+  <Route path="/" component={Inicio} />
+  <Route path="/prendas" component={Prendas} />
+  <Route path="/clientes" component={Clientes} />
+</main>
---
+++ src/Inicio.svelte
@@ -0,0 +1,37 @@
+<style>
+  h1,
+  div,
+  p {
+    margin-left: 50px;
+  }
+  div {
+    padding: 30px;
+  }
+  p {
+    padding: 10px;
+  }
+  li {
+    margin-top: 5px;
+  }
+</style>
+
+<h1>Tienda de Ropa Frontend</h1>
+<p><small><b>Progressive Web App (PWA)</b></small></p>
+<p><small><b>Código fuente en <a href="https://github.com/alvarodelmoral/ropaFrontend">https://github.com/alvarodelmoral/ropaFrontend</a></b></small></p>
+<p>Esta SPA (Single Page Application) ofrece 3 opciones:</p>
+<div>
+  <ul>
+    <li>
+      <strong>Inicio</strong>
+      : Esta página con información.
+    </li>
+    <li>
+      <strong>Prendas</strong>
+      : Permite realizar operaciones de obtención, inserción, modificación y eliminación de artículos en una BD.
+    </li>
+    <li>
+      <strong>Clientes</strong>
+      : Permite realizar operaciones de obtención, inserción, modificación y eliminación de clientes en una BD.
+  </ul>
+</div>
+<p><small>Backend en <a href="https://ropabackend.herokuapp.com">https://ropabackend.herokuapp.com</a></small></p>
